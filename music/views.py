5faeec4604674c3c21ef9484eb5be0c78bdd347d
JSkills <jskills@gmail.com>
2020-04-16 01:32:38 -0400

    display templates and assets

--- music/views.py
+++ music/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("This is only the beginning ...")
+	return render(request, 'index.html')
+
 
 
