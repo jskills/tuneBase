c12083ffc124615133e61f031e48cd8b541af1d0
JSkills <jskills@gmail.com>
2020-04-17 01:42:32 -0400

    template / view test connect raw sql call fun

--- music/urls.py
+++ music/urls.py
@@ -1,7 +1,16 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 
+admin.site.site_header = 'Music Database Administration'
+
+app_name = 'music'
+
 urlpatterns = [
+	# e.g. /music/
 	path('', views.index, name='index'),
+	#path('artist/', views.artist, name='artist'),
+	# e.g. /music/artist/1/
+	path('artist/<int:artist_id>/', views.artistPage, name='artistPage'),
 ]
 
--- music/views.py
+++ music/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
+from .models import Genre, Artist, Song
+
 def index(request):
-	return render(request, 'index.html')
+	return render(request, 'music/index.html')
+
+def artistPage(request, artist_id):
+	alist = Song.objects.raw('select * from song where artist_id = %s', [artist_id])
+	album_list = {
+		'request': request,
+		'album_list': alist
+	}
+	return render(request, 'music/artist.html', album_list)
+
+	
 
 
 
